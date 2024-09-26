86bf03a82b82252d3f88acb2fc990f3773702242
Mips2648 <mips2648@gmail.com>
2024-09-26 10:53:12 +0200

    cleaning

--- core/php/go.php
+++ core/php/go.php
@@ -15,9 +15,7 @@
  * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
  */
 
-//error_reporting(E_ALL);
-//ini_set('display_errors', 'On');
-require_once dirname(__FILE__) . "/../../../../core/php/core.inc.php";
+require_once __DIR__ . "/../../../../core/php/core.inc.php";
 
 $querystr = parse_url(urldecode($_SERVER["REQUEST_URI"]));
 parse_str($querystr['query'], $queryparam);
